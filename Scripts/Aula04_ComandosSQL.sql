da49b62c47ef0d9d7989a3755e5ff8a1592afbf3
igornatal <igorpenhanatal@hotmail.com>
2025-02-06 13:17:53 -0300

    Create Aula04_ComandosSQL.sql
    Arquivo inicial da aula.

---
+++ Scripts/Aula04_ComandosSQL.sql
@@ -0,0 +1,18 @@
+-- comentário de 1 linha
+/*
+Comentário de várias linhas
+*/
+-- ctrl + enter = executa a parte do script onde estar o cursor
+
+-- Criar a base de dados
+create database teste;
+
+-- Usar a base de dados
+use teste;
+
+-- Comando show
+-- Mostrar as bases de dados
+show schemas;
+show tables;
+
+
