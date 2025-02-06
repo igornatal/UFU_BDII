0596ed05d0ca3386a5cfa0f92d2e66ae06d173fb
igornatal <igorpenhanatal@hotmail.com>
2025-02-06 16:03:01 -0300

    Update Aula04_ComandosSQL.sql

--- Scripts/Aula04_ComandosSQL.sql
+++ Scripts/Aula04_ComandosSQL.sql
@@ -1,18 +1,29 @@
--- comentário de 1 linha
+-- Comandos: CREATE, USE, SELECT, SHOW, DESCRIBE, INSERT, UPDATE, DELETE, DROP.
+
+-- TIPOS DE DADOS: int, float, decimal, varchar, date (ano-mes-dia), blob, enum
+
+-- FORMATO DOS COMANDOS
+
 /*
-Comentário de várias linhas
-*/
--- ctrl + enter = executa a parte do script onde estar o cursor
+Parametros: primary key, auto_increment, not null
+create database **nome_do_bd**;
+use **nome_do_bd**;4
+create table **nome_tabela** (
+**atributos separados por vírgula**
+**nome_atributo1** tipo,
+**nome_atributo2** tipo
+);
 
--- Criar a base de dados
-create database teste;
+insert into **nome_tabela** values (**valor_atributo1**, **valor_atributo2**);
+insert into **nome_tablea** (**nome_atributo2**) values (**valor_atributo2**);
 
--- Usar a base de dados
-use teste;
+select * from **nome_tabela**; -- mostra todos os itens da tabela
+select (**nome_atributo2**) from **nome_tabela**; -- mostra só os atributos desejados
 
--- Comando show
--- Mostrar as bases de dados
-show schemas;
-show tables;
+update **nome_tabela**
+set **nome_atributo** = 'novo_nome'
+where **condição**
 
-
+delete from **nome_tabela**
+where **condição**
+*/ 
